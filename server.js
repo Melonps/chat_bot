f694b7ccb4773fe70850b8a8b1fd5d6a8c2854d8
Melops <95263124+Melonps@users.noreply.github.com>
2022-05-27 13:53:57 +0900

    [FIX] 基本の準備

--- server.js
+++ server.js
@@ -9,14 +9,3 @@ app.use(express.static(__dirname + '/public'));
 app.listen(config.PORT, () => {
     console.log(`Server Started on port:${config.PORT}`)
 });
-
-var server = http.createServer(
-    (request,response)=>{
-        fs.readFile('public/index.html','UTF-8',(error,data)=>{
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.write(data);
-            response.end();
-        })
-    }
-);
-//server.listen(config.PORT);
