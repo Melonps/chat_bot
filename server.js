9e905b1e236662e2b86d6ee281195a8c0f26dbd6
Melops <95263124+Melonps@users.noreply.github.com>
2022-07-06 22:42:32 +0900

    [ADD] 気象庁のAPIを叩き、表示
    やっと一歩進んだよお；；

--- server.js
+++ server.js
@@ -4,15 +4,40 @@ const fs = require('fs');
 const config = require('./server-config.json');
 const express = require('express');
 const app = express();
-
-const AREA_JSON_FILE = __dirname + '/data/area.json';
+const fetch = require('node-fetch');
+const AREA_JSON_FILE = './public/data/area.json';
+const areas = JSON.parse(fs.readFileSync(AREA_JSON_FILE, 'utf-8'));
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(config.PORT, () => {
     console.log(`Server Started on port:${config.PORT}`)
+    console.log("http://localhost:8080/");
 });
 
+app.get('/chat', async function (req, res) {
+    const text = req.query.text || '';
+    const reply = await askForWeather(text);
+    console.log(reply);
+    res.json(reply);
+});
+
+
+function textToAreaCode(text) {
+    if (!text) {
+        return  config.WEATHER_DEFAULT_AREA;
+    }
+    for (const [code, value] of Object.entries(areas.offices)) {
+        const name = value.name;
+        console.log("###### name ######");
+        console.log(name)
+        const regexp = new RegExp(text);
+        if (name.match(regexp)) {
+            return { name, code };
+        }
+    }
+    return config.WEATHER_DEFAULT_AREA;
+}
 async function askForWeather(areaName) {
     const { code, name } = textToAreaCode(areaName);
 
@@ -22,22 +47,20 @@ async function askForWeather(areaName) {
             return null;
         }
         const json = await response.json();
-
-        
+        console.log(json);
+        const latest = json[0].timeSeries[0].areas[0];
+        console.log(latest);
+        const reply = createReply(`${name}の天気は${latest.weathers[1]}でしょう`);
+        console.log(reply);
+        return reply;
+    } catch (error) {
+        console.error(error);
+        return null;
     }
 }
 
-function textToAreaCode(text) {
-    if (!text) {
-        return config.WEATHER_DEFAULT_AREA;
-    }
-    for (const [code, value] of Object.entries(area.offices)) {
-        const name = value.name;
-        console.log(name)
-        const regexp = new RegExp(text);
-        if (name.match(regexp)) {
-            return { name, code };
-        }
-    }
-    return config.WEATHER_DEFAULT_AREA;
+function createReply(text, linkUrl = null, imageUrl = null, score = -1.0) {
+    return {
+        text, linkUrl, imageUrl, score,
+    };
 }
